aaca2b4e00844683226f06e54b20aed6b1d5588d
asomensari <asomensari@newrelic.com>
2020-06-03 21:14:33 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,13 @@
+variable "env" {
+  description = "env: dev or prod"  
+}
+
+variable "appname" {
+  type          = "map"
+  description   = "Application Name"
+  default       = {
+    dev     = "My Application - Dev"
+    prod    = "My Application - Prod"
+  }
+}
 
